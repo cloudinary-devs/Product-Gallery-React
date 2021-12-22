2dfdaed48509e278eb603ebfc3966b36eed28c31
MeirFeinberg <meir.feinberg@cloudinary.com>
2021-12-22 12:48:12 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h3>Product Gallery Example</h3>
+      <div id="my-gallery"></div>
+      <p>
+      <a
+        href="https://cloudinary.com/documentation/product_gallery"
+        target="_blank">
+        Product Gallery User Guide
+      </a>
+    </p>
+    <p>
+      <a
+        href="https://cloudinary.com/documentation/product_gallery_reference"
+        target="_blank">
+        Product Gallery Reference
+      </a>
+    </p>
+    </div>
+  );
+}
